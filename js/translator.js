b3451c1f4245592928ae51c5bc24d648dbdec0d1
윤진환 <127274711+yunjinhwa@users.noreply.github.com>
2024-12-10 12:25:06 +0900

    Add files via upload

--- js/translator.js
+++ js/translator.js
@@ -4,8 +4,8 @@ const papago = document.getElementById('output-papago');
 const deepL = document.getElementById('output-deepl');
 const google_Cloud_Translate = document.getElementById('output-google');
 const microsoft_Azure = document.getElementById('output-microsoft');
-const Amazon_Translate = document.getElementById('output-amazon');
-const IBM_Watson = document.getElementById('output-IBM-Watson');
+//const Amazon_Translate = document.getElementById('output-amazon');
+//const IBM_Watson = document.getElementById('output-IBM-Watson');
 
 // select 엘리먼트 가져오기\
 const source_lang_Select = document.getElementById('source_lang');
@@ -13,13 +13,11 @@ const target_papago_Select = document.getElementById('language-papago'); // sele
 const target_deepl_Select = document.getElementById('language-deepl');
 const target_google_Select = document.getElementById('language-google');
 const target_microsoft_Select = document.getElementById('language-microsoft');
-const target_amazon_Select = document.getElementById('language-amazon');
-const target_IBM_Watson_Select = document.getElementById('language-IBM-Watson');
+//const target_amazon_Select = document.getElementById('language-amazon');
+//const target_IBM_Watson_Select = document.getElementById('language-IBM-Watson');
 
 // select 값 가져오는 방법 1: value 속성 사용
-const microsoft_selected_Value = target_microsoft_Select.value;
-const amazon_selected_Value = target_amazon_Select.value;
-const IBM_Watson_selected_Value = target_IBM_Watson_Select.value;
+//const amazon_selected_Value = target_amazon_Select.value;
 
 // select 값 가져오는 방법 2: 선택된 option의 텍스트 가져오기
 //const selectedText = targetdeeplSelect.options[targetdeeplSelect.selectedIndex].text;
@@ -27,58 +25,33 @@ const IBM_Watson_selected_Value = target_IBM_Watson_Select.value;
 //papago 번역기
 //CORS 정책 떄문에 서버를 거치지 않으면 사용 불가
 //Node.js 필요
-async function detectLanguageWithPapago(text) {
-    const papago_client_id = 'ogwo51ja4q';
-    const papago_apiKey = 'pObTOdYCeFpFbyfvwb4dDrNNLkzuZ7EbyJrjBSAP';
-    const papago_url = 'https://naveropenapi.apigw.ntruss.com/nmt/v1/translate';
-
-    try{
-        const response = await fetch(papago_url, {
-            method: 'POST',
-            headers: {
-                'X-NCP-APIGW-API-KEY-ID': papago_client_id,
-                'X-NCP-APIGW-API-KEY': papago_apiKey,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({
-                text: text,
-            }).toString()
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data.message.result.langCode;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
 async function translateWithPapago(text, sourceLang, targetLang) {
     const papago_client_id = 'ogwo51ja4q';
     const papago_apiKey = 'pObTOdYCeFpFbyfvwb4dDrNNLkzuZ7EbyJrjBSAP';
-    const papago_url = 'https://naveropenapi.apigw.ntruss.com/nmt/v1/translate';
+    const proxy_url = 'https://corsproxy.io/?';
+    const papago_url = 'https://naveropenapi.apigw.ntruss.com/nmt/v1/translation';
 
-    if(sourceLang == "auto"){
-        sourceLang = await detectLanguageWithPapago(text);
-        if (!sourceLang) return;
-    }
+    // if(sourceLang == "auto"){
+    //     sourceLang = await detectLanguageWithPapago(text);
+    //     if (!sourceLang) return;
+    // }
+
+    const params = new URLSearchParams({
+        source: sourceLang,
+        target: targetLang,
+        text: text,
+    });
 
     try{
         const response = await fetch(papago_url, {
             method: 'POST',
             headers: {
+                'Access-Control-Allow-Origin': '*',
                 'X-NCP-APIGW-API-KEY-ID': papago_client_id,
                 'X-NCP-APIGW-API-KEY': papago_apiKey,
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            body: new URLSearchParams({
-                source: sourceLang,
-                target: targetLang,
-                text: text,
-            }).toString()
+            body: params
         });
 
         if (!response.ok) {
@@ -162,7 +135,6 @@ async function detectLanguageWithGoogle(text) {
         return null;
     }
 }
-
 //번역
 async function translateWithGoogle(text, sourceLang, targetLang) {
     const apiKey = 'AIzaSyAsB4qjZj9ZRcTNsysdVeKJlVwcdKpTLBs'; // Google Cloud API 키
@@ -206,29 +178,58 @@ async function translateWithGoogle(text, sourceLang, targetLang) {
     }
 }
 
-
-
 //microsoft 번역기
-async function translateWithMicrosoft(text, sourceLang, targetLang) {
-    const microsoft_apiKey = 'nxpnJSDSjMlIOLah42fnkSqng3VMCuMyX8AM86B6Dv6FvSGmwwtuJQQJ99ALACNns7RXJ3w3AAAbACOGrkr3';
-    const microsoft_url = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0';
+//언어감지
+async function detectLanguageWithMicrosoftAzure(text) {
+    const microsoftApiKey = '3uwlZXwyqNDSgQMgLkOlnLeYlOeL8Ek83rumRHdnACtOFSHtOUitJQQJ99ALACNns7RXJ3w3AAAbACOGnPrY'; // 여기에 Azure API 키를 입력하세요
+    const microsoftUrl = 'https://api.cognitive.microsofttranslator.com/detect?api-version=3.0';
 
     const requestBody = [{
-        text: text,
-        from: sourceLang,
-        to: targetLang,
+        text: text
+    }];
+
+    try {
+        const response = await fetch(microsoftUrl, {
+            method: 'POST',
+            headers: {
+                'Ocp-Apim-Subscription-Key': microsoftApiKey,
+                'Content-Type': 'application/json',
+                'Ocp-Apim-Subscription-Region': 'koreacentral', // 여기에 Azure 리전을 입력하세요
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(`HTTP error! Status: ${response.status}, Message: ${errorMessage}`);
+        }
+
+        const data = await response.json();
+        return data[0].language; // 감지된 언어 반환
+    } catch (error) {
+        console.error('언어 감지 오류:', error);
+        return null;
+    }
+}
+
+//번역기
+async function translateWithMicrosoft(text, sourceLang, targetLang) {
+    const microsoft_apiKey = '3uwlZXwyqNDSgQMgLkOlnLeYlOeL8Ek83rumRHdnACtOFSHtOUitJQQJ99ALACNns7RXJ3w3AAAbACOGnPrY';
+    if(sourceLang == "auto"){
+        sourceLang = await detectLanguageWithMicrosoftAzure(text);
+        if (!sourceLang) return;
+    }
+
+    const microsoft_url = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=' + sourceLang + '&to=' + targetLang;
+
+    const requestBody = [{
+        text: text
     }];
 
     if(sourceLang == targetLang){
         microsoft_Azure.textContent = text;
         return;
     }
-
-    console.log(requestBody);
-    console.log('Request URL:', microsoft_url); // 요청 URL 로그 출력
-    console.log('Request Body:', JSON.stringify(requestBody)); // 요청 본문 로그 출력
-    console.log('API Key:', microsoft_apiKey); // API 키 로그 출력
-    console.log('Region:', 'koreacentral'); // 지역 확인
 
     try{
         const response = await fetch(microsoft_url, {
@@ -253,34 +254,22 @@ async function translateWithMicrosoft(text, sourceLang, targetLang) {
 }
 
 //amazon 번역기
-async function translateWithAmazon(text, sourceLang, targetLang) {
-    const amazon_apiKey = 'b2554f21-6815-4c7e-9fe0-7e99107f3289:fx';
-}
+// async function translateWithAmazon(text, sourceLang, targetLang) {
+//     const amazon_apiKey = 'b2554f21-6815-4c7e-9fe0-7e99107f3289:fx';
+// }
 
-//IBM-Watson 번역기
-async function translateWithIBM_Watson(text, sourceLang, targetLang) {
-    const IBM_Watson_apiKey = 'b2554f21-6815-4c7e-9fe0-7e99107f3289:fx';
-}
-
-// 사용 예:
-//translateWithDeepL(inputText.value, 'EN'); // 한국어 -> 영어 번역
-
-// select 값 변경 감지하기
-// targetdeeplSelect.addEventListener('change', (event) => {
-//     const selectedValue = event.target.value;
-//     console.log('선택된 언어:', selectedValue);
-    
-//     // 여기서 번역 함수 호출
-//     translateWithDeepL(inputText.value, selectedValue);
-// });
+// //IBM-Watson 번역기
+// async function translateWithIBM_Watson(text, sourceLang, targetLang) {
+//     const IBM_Watson_apiKey = 'b2554f21-6815-4c7e-9fe0-7e99107f3289:fx';
+// }
 
 translate_Button.addEventListener('click', () => {
     const input_lang = input_Text.value;
     const source_lang_selected_Value = source_lang_Select.value;
     
     //papago 번역기
-    //const papago_selected_Value = target_papago_Select.value;
-    //translateWithPapago(input_lang, source_lang_selected_Value, papago_selected_Value);
+    const papago_selected_Value = target_papago_Select.value;
+    translateWithPapago(input_lang, source_lang_selected_Value, papago_selected_Value);
 
     //deepL 번역기
     const deepl_selected_Value = target_deepl_Select.value;
@@ -292,6 +281,7 @@ translate_Button.addEventListener('click', () => {
 
     //microsoft 번역기
     const microsoft_selected_Value = target_microsoft_Select.value;
+    //console.log(detectLanguageWithMicrosoftAzure(input_lang));
     translateWithMicrosoft(input_lang, source_lang_selected_Value, microsoft_selected_Value);
 });
 
@@ -300,6 +290,9 @@ target_deepl_Select.addEventListener('change', (e) => {
     const input_lang = input_Text.value;
     const source_lang_selected_Value = source_lang_Select.value;
     const selectedValue = e.target.value;
+    if(input_lang == ''){
+        return;
+    }
     translateWithDeepL(input_lang, source_lang_selected_Value, selectedValue);
 });
 
@@ -308,6 +301,9 @@ target_google_Select.addEventListener('change', (e) => {
     const input_lang = input_Text.value;
     const source_lang_selected_Value = source_lang_Select.value;
     const selectedValue = e.target.value;
+    if(input_lang == ''){
+        return;
+    }
     translateWithGoogle(input_lang, source_lang_selected_Value, selectedValue);
 });
 
@@ -316,6 +312,9 @@ target_microsoft_Select.addEventListener('change', (e) => {
     const input_lang = input_Text.value;
     const source_lang_selected_Value = source_lang_Select.value;
     const selectedValue = e.target.value;
+    if(input_lang == ''){
+        return;
+    }
     translateWithMicrosoft(input_lang, source_lang_selected_Value, selectedValue);
 });
 
